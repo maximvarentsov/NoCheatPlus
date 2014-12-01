3145b221b240c9752cc5c93f1a132b367ce145d1
Maxim Varentsov <maxim.v@ohm.pw>
2014-12-01 14:17:27 +0300

    Unify and simplify Check / AsyncCheck, ACheckConfig / AsyncCheckConfig. Hinting at further changes to unify permission access and caching. Later permission caching can also be done for moving checks, though checking for the current thread may be exchanged in favor of config flags. Generic configuration will allow to set individual timeouts for checks, check groups and default. There could also be change policies like "world change only", possibly listening to permission updates, if feasible.
    CheckData / AsyncCheckData are not as simple to unify, if we want to
    keep the footprint as low as possible there. There could even be more
    implementations to match different policies.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/Check.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/Check.java
@@ -3,6 +3,7 @@ package fr.neatmonster.nocheatplus.checks;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import fr.neatmonster.nocheatplus.NCPAPIProvider;
@@ -27,7 +28,7 @@ public abstract class Check implements MCAccessHolder{
 
     /**
      * Gets the player's history.
-     * 
+     *
      * @param player
      *            the player
      * @return the history
@@ -40,12 +41,12 @@ public abstract class Check implements MCAccessHolder{
 
     /** The type. */
     protected final CheckType type;
-    
+
     protected MCAccess mcAccess;
 
     /**
      * Instantiates a new check.
-     * 
+     *
      * @param type
      *            the type
      */
@@ -55,11 +56,11 @@ public abstract class Check implements MCAccessHolder{
         ViolationHistory.checkTypeMap.put(getClass().getName(), type);
         DataManager.registerExecutionHistory(type, histories);
     }
-    
+
     /**
      * Execute actions, possibly thread safe according to the isMainThread flag.<br>
      * This does not use extra synchronization.
-     * 
+     *
      * @param player
      *            the player
      * @param VL
@@ -71,14 +72,14 @@ public abstract class Check implements MCAccessHolder{
      * @return true, if successful
      */
     public boolean executeActions(final Player player, final double vL, final double addedVL,
-            final ActionList actions, boolean isMainThread) {
+                                  final ActionList actions, boolean isMainThread) {
         // Sync it into the main thread by using an event.
         return executeActions(new ViolationData(this, player, vL, addedVL, actions), isMainThread);
     }
 
     /**
      * Convenience method: Execute actions from the main thread only.
-     * 
+     *
      * @param player
      *            the player
      * @param vL
@@ -90,64 +91,64 @@ public abstract class Check implements MCAccessHolder{
      * @return true, if the event should be cancelled
      */
     protected boolean executeActions(final Player player, final double vL, final double addedVL,
-            final ActionList actions) {
+                                     final ActionList actions) {
         return executeActions(new ViolationData(this, player, vL, addedVL, actions), true);
     }
-    
+
     /**
      * Execute some actions for the specified player, only for the main thread.
-     * 
+     *
      * @param violationData
      *            the violation data
      * @return true, if the event should be cancelled
      */
     protected boolean executeActions(final ViolationData violationData){
-    	return executeActions(violationData, true);
+        return executeActions(violationData, true);
     }
 
     /**
      * Execute some actions for the specified player.
-     * 
+     *
      * @param violationData
      *            the violation data
      * @param isMainThread If this is called from within the main thread. If true, this must really be the main thread and not from synchronized code coming form another thread.
      * @return true, if the event should be cancelled
      */
     protected boolean executeActions(final ViolationData violationData, final boolean isMainThread) {
-    	
-    	// Dispatch the VL processing to the hook manager (now thread safe).
+
+        // Dispatch the VL processing to the hook manager (now thread safe).
         if (NCPHookManager.shouldCancelVLProcessing(violationData))
             // One of the hooks has decided to cancel the VL processing, return false.
             return false;
-   
-        final boolean hasCancel = violationData.hasCancel(); 
-    	
-        if (isMainThread) 
-        	return violationData.executeActions();
-        else 
-        	// Always schedule to add to ViolationHistory.
-        	TickTask.requestActionsExecution(violationData);
-        
-    	// (Design change: Permission checks are moved to cached permissions, lazily updated.)
-    	return hasCancel;
+
+        final boolean hasCancel = violationData.hasCancel();
+
+        if (isMainThread)
+            return violationData.executeActions();
+        else
+            // Always schedule to add to ViolationHistory.
+            TickTask.requestActionsExecution(violationData);
+
+        // (Design change: Permission checks are moved to cached permissions, lazily updated.)
+        return hasCancel;
     }
-    
+
     /**
      * Fill in parameters for creating violation data. 
      * Individual checks should override this to fill in check specific parameters,
      * which then are fetched by the violation data instance.
-     * @param player
+     * @param violationData
      * @return
      */
     protected Map<ParameterName, String> getParameterMap(final ViolationData violationData){
-    	final Map<ParameterName, String> params = new HashMap<ParameterName, String>();
-    	// (Standard parameters like player, vl, check name are filled in in ViolationData.getParameter!)
-    	return params;
+        final Map<ParameterName, String> params = new HashMap<ParameterName, String>();
+        // (Standard parameters like player, vl, check name are filled in in ViolationData.getParameter!)
+        return params;
     }
 
     /**
      * Gets the type of the check.
-     * 
+     *
      * @return the type
      */
     public CheckType getType() {
@@ -156,30 +157,37 @@ public abstract class Check implements MCAccessHolder{
 
     /**
      * Checks if this check is enabled for the specified player.
-     * 
+     *
      * @param player
      *            the player
      * @return true, if the check is enabled
      */
     public boolean isEnabled(final Player player) {
-        try {
-            if (!type.isEnabled(player) || player.hasPermission(type.getPermission()))
+        if (!type.isEnabled(player)) {
+            return false;
+        }
+        // TODO: Checking for the thread might be a temporary measure.
+        if (Bukkit.isPrimaryThread()) {
+            // Check permissions directly.
+            if (player.hasPermission(type.getPermission())) {
                 return false;
-        } catch (final Exception e) {
-        	// TODO: this should be mostly obsolete.
-        	LogUtil.logSevere(e);
+            }
+        }
+        else if (type.hasCachedPermission(player)) {
+            // Assume asynchronously running check.
+            return false;
         }
         return !NCPExemptionManager.isExempted(player, type);
     }
 
-	@Override
-	public void setMCAccess(MCAccess mcAccess) {
-		this.mcAccess = mcAccess;
-	}
+    @Override
+    public void setMCAccess(MCAccess mcAccess) {
+        this.mcAccess = mcAccess;
+    }
 
-	@Override
-	public MCAccess getMCAccess() {
-		return mcAccess;
-	}
-	
+    @Override
+    public MCAccess getMCAccess() {
+        return mcAccess;
+    }
+
 }
--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/access/ACheckConfig.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/access/ACheckConfig.java
@@ -12,26 +12,40 @@ public abstract class ACheckConfig implements ICheckConfig {
 
     /** For on the fly debug setting. */
     public boolean debug = false;
-    
-    /** For adaption to server side lag. */
+
+    /** If to adapt to server side lag. */
     public final boolean lag;
-    
+
+    /** Permissions to hold in player data cache, not final for flexibility. */
+    protected String[] cachePermissions;
+
     /**
-     * 
+     *
      * @param config
      * @param pathPrefix Path prefix for the check section (example for use: prefix+"debug").
      */
     public ACheckConfig(final ConfigFile config, final String pathPrefix){
-    	// TODO: Path prefix construction is somewhat inconsistent with debug hierarchy ?
+        this(config, pathPrefix, null);
+    }
+
+    /**
+     *
+     * @param config
+     * @param pathPrefix Path prefix for the check section (example for use: prefix+"debug").
+     * @param cachePermissions  cachePermissions Permissions to hold in player data cache. Can be null.
+     */
+    public ACheckConfig(final ConfigFile config, final String pathPrefix, final String[] cachePermissions){
+        // TODO: Path prefix construction is somewhat inconsistent with debug hierarchy ?
         debug = config.getBoolean(pathPrefix + ConfPaths.SUB_DEBUG, config.getBoolean(ConfPaths.CHECKS_DEBUG, false));
         // TODO: Use lag flag where appropriate and document it (or get rid of it).
         lag = config.getBoolean(pathPrefix + ConfPaths.SUB_LAG, true) && config.getBoolean(ConfPaths.MISCELLANEOUS_LAG, true);
+        this.cachePermissions = cachePermissions;
     }
-    
-	@Override
-	public String[] getCachePermissions() {
-		return null;
-	}
+
+    @Override
+    public String[] getCachePermissions() {
+        return cachePermissions;
+    }
 
     @Override
     public boolean getDebug() {
@@ -42,5 +56,5 @@ public abstract class ACheckConfig implements ICheckConfig {
     public void setDebug(final boolean debug) {
         this.debug = debug;
     }
-	
+
 }
