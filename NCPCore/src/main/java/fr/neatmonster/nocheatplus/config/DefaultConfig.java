4c4e646635a285fc38a14f290a2d7ea93718ac9d
Maxim Varentsov <maxim.v@ohm.pw>
2014-12-02 03:44:27 +0300

    The server logger should be considered thread-safe by default (log4j). To be sure that we don't kill exotic systems, it's also configurable.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/config/DefaultConfig.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/config/DefaultConfig.java
@@ -38,6 +38,7 @@ public class DefaultConfig extends ConfigFile {
                 set(ConfPaths.LOGGING_DEBUG, false);
                 set(ConfPaths.LOGGING_BACKEND_CONSOLE_ACTIVE, true);
                 set(ConfPaths.LOGGING_BACKEND_CONSOLE_PREFIX, "[NoCheatPlus] ");
+                set(ConfPaths.LOGGING_BACKEND_CONSOLE_ASYNCHRONOUS, true);
                 set(ConfPaths.LOGGING_BACKEND_FILE_ACTIVE, true);
                 set(ConfPaths.LOGGING_BACKEND_FILE_PREFIX, "");
                 set(ConfPaths.LOGGING_BACKEND_FILE_FILENAME, "nocheatplus.log");
